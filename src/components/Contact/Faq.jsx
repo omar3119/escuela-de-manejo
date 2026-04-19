28e4c3549358efbeeb5f4f63062a5b0aaf5c15d7
Omar <omarreyez31@gmail.com>
2026-04-19 09:17:21 -0400

    add cursor pointer faq

--- src/components/Contact/Faq.jsx
+++ src/components/Contact/Faq.jsx
@@ -1,10 +1,10 @@
 import { IoIosArrowForward } from "react-icons/io";
 const Faq = ({ question, answer, isOpen, onClic }) => {
   return (
-    <div className="bg-white rounded-xl border-gray-200 p-6 ">
+    <div className="bg-white rounded-xl border-gray-200 p-6 cursor-pointer">
       <button
         onClick={onClic}
-        className="w-full flex justify-between items-center text-left font-medium text-gray-900 hover:text-gray-600 transition-colors"
+        className="w-full cursor-pointer flex justify-between items-center text-left font-medium text-gray-900 hover:text-gray-600 transition-colors"
       >
         <span className="text-primary font-bold text-base">{question}</span>
         <div>
