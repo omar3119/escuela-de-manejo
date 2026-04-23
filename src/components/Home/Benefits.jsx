0a49293b867107f001c868625c6baa45eb5ba428
Omar <omarreyez31@gmail.com>
2026-04-23 17:56:33 -0500

    Update benefit card size, text, and icon

--- src/components/Home/Benefits.jsx
+++ src/components/Home/Benefits.jsx
@@ -2,17 +2,17 @@ import { FaCalendarAlt } from "react-icons/fa";
 
 const Benefits = ({title, description, icon}) => {
   return (
-    <div className="w-full p-8  bg-[#F2F4F6] rounded-lg md:flex md:justify-center lg:max-w-95.5">
+    <div className="w-full p-6  bg-[#e2e4e6] rounded-lg md:flex md:justify-center lg:max-w-95.5">
       <div className="md:max-w-130 ">
-        <div className="inline-block bg-[#1e40af18] p-4 rounded-2xl">
+        <div className="inline-block bg-[#1e40af18] p-2 rounded-lg">
           {
             icon
           }
         </div>
 
         <div>
-          <h3 className="font-bold text-xl py-4">{title}</h3>
-          <p className="text-[#444653]">
+          <h3 className="font-bold text-lg py-2">{title}</h3>
+          <p className="text-[#444653] text-sm leading-6">
            {description}
           </p>
         </div>
