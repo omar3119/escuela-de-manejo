957a37e5b7d46ee4c0bd646270e545445a5ace1d
Omar <omarreyez31@gmail.com>
2026-04-24 16:45:11 -0500

    update btn color

--- src/components/BtnWhatsapp.jsx
+++ src/components/BtnWhatsapp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const BtnWhatsapp = ()=>{
     return(
-        <div className="fixed bottom-5 right-3 lg:bottom-10 lg:right-10 bg-[#1ca24f] p-3 rounded-full z-50" >
+        <div className="fixed bottom-5 right-3 lg:bottom-10 lg:right-10 bg-[#39dd56] p-3 rounded-full z-50" >
         <FaWhatsapp className=" text-white text-3xl lg:text-6xl cursor-pointer" title="WhatsApp"/>
 
         </div>
