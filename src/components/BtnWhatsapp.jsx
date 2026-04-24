6fbefcca3401d982b6026fc65516551d35a9d1f9
Omar <omarreyez31@gmail.com>
2026-04-24 16:41:03 -0500

    Update WhatsApp button size

--- src/components/BtnWhatsapp.jsx
+++ src/components/BtnWhatsapp.jsx
@@ -3,8 +3,8 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const BtnWhatsapp = ()=>{
     return(
-        <div className="fixed bottom-5 right-3 lg:bottom-10 lg:right-10 bg-green-600 p-2 rounded-2xl z-50" >
-        <FaWhatsapp className=" text-white text-5xl lg:text-6xl cursor-pointer" title="WhatsApp"/>
+        <div className="fixed bottom-5 right-3 lg:bottom-10 lg:right-10 bg-[#1ca24f] p-3 rounded-full z-50" >
+        <FaWhatsapp className=" text-white text-3xl lg:text-6xl cursor-pointer" title="WhatsApp"/>
 
         </div>
     )
