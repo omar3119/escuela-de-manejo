c21cb7676c1f6ed030f8fa685f7c23d0135b6d81
Omar <omarreyez31@gmail.com>
2026-04-23 21:23:21 -0500

    add btn plasns

--- src/pages/PricingPlans.jsx
+++ src/pages/PricingPlans.jsx
@@ -1,5 +1,3 @@
-
-
 //COMPONENTS------->
 import Card from "../components/Courses/CardServices";
 import HeroMain from "../components/HeroMain";
@@ -9,12 +7,27 @@ import imgHeroPlans from "./../assets/images/img-hero-plans.png";
 import HeroLayot from "../Layout/HeroLayout";
 //DATA------->
 import datosPlanes from "../data/datosPlanes";
-
+//ICONS
+import { FaCar } from "react-icons/fa";
+import { FaMotorcycle } from "react-icons/fa6";
 
 function PricingPlans() {
+  const plans = [
+    {
+      id: 1,
+      management: "Auto",
+      u: "/auto",
+      icon: <FaCar />,
+    },
+    {
+      id: 2,
+      management: "Moto",
+      u: "/moto",
+      icon: <FaMotorcycle />,
+    },
+  ];
   return (
     <div className="w-full md:flex md:flex-col md:items-center">
-
       {/* //HERO----- */}
       <div className="w-full md:flex md:justify-center relative ">
         <div className="w-full h-full bg-[#000000a8] absolute z-50 bg-linear-to-r from-[#01051a] via-[#041329ad] to-[#04132900]"></div>
@@ -53,18 +66,32 @@ function PricingPlans() {
             </p>
           </div>
 
-          <div className="w-full flex flex-col gap-6 lg:w-full lg:flex-row">
-            {datosPlanes.map((plan) => (
-              <Card
-                key={plan.id}
-                title={plan.title}
-                description={plan.description}
-                price={plan.price}
-                datos={plan.features}
-                accent={plan.color}
+          <div className="">
+            <div className="w-full flex justify-center pb-10">
+              <div className="bg-shadow-black shadow-2xl inline-flex px-4 py-2 justify-center gap-2  rounded-lg">
+                {plans.map((item, i) => (
+                  <div className="">
+                    <button className="flex items-center gap-2 py-2  bg-primary text-white rounded-lg px-4 cursor-pointer" key={i}>
+                    {item.icon}
 
-              />
-            ))}
+                      {item.management}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="w-full flex flex-col gap-6 lg:w-full lg:flex-row">
+              {datosPlanes.map((plan) => (
+                <Card
+                  key={plan.id}
+                  title={plan.title}
+                  description={plan.description}
+                  price={plan.price}
+                  datos={plan.features}
+                  accent={plan.color}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
