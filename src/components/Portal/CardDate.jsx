8a4959dc8fed159efc21ddd0e82446e6cd93fea0
Omar <omarreyez31@gmail.com>
2026-05-01 19:34:35 -0500

    update cardDate bgColor

--- src/components/Portal/CardDate.jsx
+++ src/components/Portal/CardDate.jsx
@@ -3,7 +3,7 @@ const CardDate = ({day, dayNum}) => {
   const hoy = date.getDate();
 
   return (
-    <div className={`${day == hoy ? "bg-[#b9c3f8] text-[#003FB1] " : "text-black bg-[#F0F3FF]"}  flex  items-center justify-center text-center bg-[#0d0d0d28] border border-[#C3C5D7] rounded-lg py-2 mb-3 gap-2`}>
+    <div className={`${day == hoy ? "bg-primary text-white" : "text-black bg-[#F0F3FF]"}  flex  items-center justify-center text-center bg-[#0d0d0d28] border border-[#dee0ec] rounded-sm py-2 mb-3 gap-2`}>
       <p className="text-sm font-semibold uppercase tracking-widest">
         {dayNum}
       </p>
